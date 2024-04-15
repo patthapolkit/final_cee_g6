e930ed20c77d12bbe469418de10cb0bd3fdebbbb
Thiraput01 <Thiraputball@gmail.com>
2024-04-15 14:23:00 +0700

    connected MongoDB

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,3 +1,9 @@
 import mongoose from "mongoose";
 
-mongoose.connect(process.env.MONGO_URL);
+const connectDB = async ()=>{
+    console.log('trying to connect MongoDB....');
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB connected: ${conn.connection.host}`);
+}
+
+export default connectDB;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,19 +1,30 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import connectDB from './config/db.js';
 
-//Route files
+
+dotenv.config({ path: './src/config/config.env' });
+
+// Route files
 import golfballs from './routes/golfball.js';
 
-//Load env vars
-dotenv.config({ path: './config/config.env'});
+// Connect to MongoDB
+connectDB();
 
 const app = express();
 
-app.use('/api/golfballs', golfballs)
 app.use(cors());
 
+// Define routes
+app.use('/api/golfballs', golfballs);
 
+const PORT = process.env.PORT || 3222;
 
-const PORT = process.env.PORT || 3222
-app.listen(PORT, console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+// Start server
+const server = app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+process.on('unhandledRejection', (err, promise)=>{
+    console.log(`Error: ${err.message}`);
+
+    server.close(()=> process.exit(1));
+})
