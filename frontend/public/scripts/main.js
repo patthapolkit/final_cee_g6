0b4483650094087321b04f7d3b8272746d1ab347
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-04-20 01:32:14 +0700

    fix join room and create room button fuctionallity

--- frontend/public/scripts/main.js
+++ frontend/public/scripts/main.js
@@ -1,67 +1,73 @@
-import { createRoom, createUser, createInstance, getRoomNumber } from "./api.js";
+import {
+  createRoom,
+  getAllRooms,
+  getAllRoomNumber,
+  getRoomById,
+  deleteRoomById,
+  createInstance,
+  updateInstance,
+  deleteInstance,
+  getUsers,
+  createUser,
+  getUserById,
+} from "./api.js";
 
 const joinRoomButton = document.getElementById("joinRoomButton");
+const createRoomButton = document.getElementById("createRoomButton");
 const nameInput = document.getElementById("nameInput");
 const roomNumberInput = document.getElementById("roomNumberInput");
+
 joinRoomButton.addEventListener("click", async () => {
   const createdUser = await createUser({
     name: nameInput.value,
     roomNumber: roomNumberInput.value,
   });
-
   const userId = createdUser.data._id;
-  const data = await getRoomNumber()
-  const roomId = data.data.find((room) => room.roomNumber === Number(roomNumberInput.value))
-  console.log(userId)
-  console.log(roomId._id)
-  console.log(data.data)
-  console.log(typeof(roomNumberInput.value))
-  await createInstance(roomId._id, {
+  const rooms = await getAllRoomNumber();
+  const room = rooms.data.find(
+    (room) => room.roomNumber === Number(roomNumberInput.value)
+  );
+  const roomId = room.roomId;
+
+  await createInstance(roomId, {
     player: userId,
     current_swings: 0,
     total_swings: 0,
     current_position: {
-      posX: 18,
-      posY: 18
-    }
-  })
+      posX: 100,
+      posY: 100,
+    },
+  });
 
   nameInput.value = "";
   roomNumberInput.value = "";
-
 });
 
- const createRoomButton = document.getElementById("createRoomButton");
- createRoomButton.addEventListener("click", async () => {
-/*    await createUser({
-     name: nameInput.value,
-     roomNumber: roomNumberInput.value,
-   });
-   nameInput.value = ""; */
+createRoomButton.addEventListener("click", async () => {
+  const createdUser = await createUser({
+    name: nameInput.value,
+    roomNumber: roomNumberInput.value,
+  });
+  const userId = createdUser.data._id;
 
-   const createdUser = await createUser({
-      name: nameInput.value,
-      roomNumber: roomNumberInput.value,
-    });
-    console.log(typeof(createdUser.data._id));
-    const userId = createdUser.data._id;
-    nameInput.value = "";
-
-   await createRoom({
-      roomNumber: roomNumberInput.value,
-      status: "waiting",
-      numberOfPlayers: 1,
-      playerTurn: userId,
-      Instance: [{
+  await createRoom({
+    roomNumber: roomNumberInput.value,
+    status: "waiting",
+    numberOfPlayers: 1,
+    playerTurn: userId,
+    Instance: [
+      {
         player: userId,
         current_swings: 0,
         total_swings: 0,
         current_position: {
-          posX: 18,
-          posY: 18
-        }
-      }]
-   })
-   roomNumberInput.value = "";
+          posX: 100,
+          posY: 100,
+        },
+      },
+    ],
+  });
 
- });
+  nameInput.value = "";
+  roomNumberInput.value = "";
+});
