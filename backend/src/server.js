b894cecb9e76b48425383dc7f91d4656d3ab08fa
Thiraput01 <Thiraputball@gmail.com>
2024-04-20 21:51:20 +0700

    remove route for game.js && golfball.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,13 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-import connectDB from './config/db.js';
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import connectDB from "./config/db.js";
 
-
-dotenv.config({ path: './src/config/config.env'});
+dotenv.config({ path: "./src/config/config.env" });
 
 // Route files
-import golfballs from './routes/golfball.js';
-import user from './routes/user.js';
-import game from './routes/game.js';
-import room from './routes/room.js';
+import user from "./routes/user.js";
+import room from "./routes/room.js";
 
 // Connect to MongoDB
 connectDB();
@@ -20,17 +17,19 @@ app.use(express.json());
 app.use(cors());
 
 // Define routes
-app.use('/api/golfballs', golfballs);
-app.use('/api/user', user);
-app.use('/api/game', game);
-app.use('/api/room', room);
+app.use("/api/user", user);
+app.use("/api/room", room);
 
 const PORT = process.env.PORT || 3222;
 
 // Start server
-const server = app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
-process.on('unhandledRejection', (err, promise)=>{
-    console.log(`Error: ${err.message}`);
+const server = app.listen(PORT, () =>
+  console.log(
+    `Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`
+  )
+);
+process.on("unhandledRejection", (err, promise) => {
+  console.log(`Error: ${err.message}`);
 
-    server.close(()=> process.exit(1));
-})
+  server.close(() => process.exit(1));
+});
