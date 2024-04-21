c01e3ba3aac2fba7be347c7300dfe54a9ed05f72
Thiraput01 <Thiraputball@gmail.com>
2024-04-21 18:03:23 +0700

    added some style

--- backend/src/controllers/user.js
+++ backend/src/controllers/user.js
@@ -3,37 +3,36 @@ import User from "../models/user.js";
 //@desc    Create User
 //@route   POST /api/users
 export const createUser = async (req, res) => {
-    try {
-        const user = await User.create(req.body);
-        res.status(201).json({success: true,  data: user});
-    } catch (error) {
-      res.status(400).json({success: false, message: "error"});
-    }
+  try {
+    const user = await User.create(req.body);
+    res.status(201).json({ success: true, data: user });
+  } catch (error) {
+    res.status(400).json({ success: false, message: "please type user" });
+  }
 };
 
 //@desc    Get All Users
 //@route   GET /api/users
 export const getAllUsers = async (req, res) => {
-    try {
-        const users = await User.find();
-        res.status(200).json({success: true, count: users.length, data: users});
-    } catch (error) {
-      res.status(400).json({success: false, message: "error"});
-    }
+  try {
+    const users = await User.find();
+    res.status(200).json({ success: true, count: users.length, data: users });
+  } catch (error) {
+    res.status(400).json({ success: false, message: "error occored" });
+  }
 };
-
 
 //@desc    Get One Users
 //@route   GET /api/users/id
 export const getUserbyId = async (req, res) => {
   try {
-      const user = await User.findById(req.params.id);
-      if(!user){
-        res.status(404).json({success: false, message: "cannot find user"})
-      }
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      res.status(404).json({ success: false, message: "cannot find user" });
+    }
 
-      res.status(200).json({success: true, data: user});
+    res.status(200).json({ success: true, data: user });
   } catch (error) {
-    res.status(400).json({success: false, message: "error"});
+    res.status(400).json({ success: false, message: "please type a user" });
   }
 };
