c91027b581359d9c80f4954a01174daee3911b95
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-04-24 03:20:11 +0700

    style

--- frontend/public/scripts/leaderboard.js
+++ frontend/public/scripts/leaderboard.js
@@ -9,31 +9,35 @@ setInterval(async () => {
   for (const info of dataList) {
     const playerdata = await getUserById(info.player);
     const playerName = playerdata.data.name;
-    const current_map = (await getPlayerControlbyId(playerdata.data._id)).data.currentMap;
+    const current_map = (await getPlayerControlbyId(playerdata.data._id)).data
+      .currentMap;
     swingsData.push({ playerName, swings: info.total_swings, current_map });
   }
 
+  function createLeaderboard(data) {
+    const leaderboardContainer = document.getElementById(
+      "leaderboard-container"
+    );
+    leaderboardContainer.innerHTML = `<h1 class="leaderboard-title">Leaderboard</h1>`;
 
-function createLeaderboard(data) {
-  const leaderboardContainer = document.getElementById("leaderboard-container");
-  leaderboardContainer.innerHTML = "<h1>Leaderboard</h1>";
+    data.sort((a, b) => {
+      if (a.current_map < b.current_map) return 1;
+      if (a.current_map > b.current_map) return -1;
 
-  data.sort((a, b) => {
-    if (a.current_map < b.current_map) return 1;
-    if (a.current_map > b.current_map) return -1;
-  
-    if (a.swings < b.swings) return -1;
-    if (a.swings > b.swings) return 1;
-  
-    return 0;
-  });
+      if (a.swings < b.swings) return -1;
+      if (a.swings > b.swings) return 1;
 
-  data.forEach((player, index) => {
-    const listItem = document.createElement("div");
-    listItem.textContent = `${index + 1}. ${player.playerName} - ${player.swings} swings - Map ${player.current_map}`;
-    leaderboardContainer.appendChild(listItem);
-  });
-}
+      return 0;
+    });
+
+    data.forEach((player, index) => {
+      const listItem = document.createElement("div");
+      listItem.innerHTML = `<p>${index + 1}. ${player.playerName}</p><p>${
+        player.swings
+      } Swings - Map ${player.current_map}</p>`;
+      leaderboardContainer.appendChild(listItem);
+    });
+  }
 
   createLeaderboard(swingsData);
 }, 1000);
