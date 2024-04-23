4c4218c712ba897ceff2cc9fc0e5948bdcd1d247
Patthapol Kittikun <6530285121@student.chula.ac.th>
2024-04-23 23:48:39 +0700

    Refactor leaderboard UI and styles

--- frontend/public/scripts/leaderboard.js
+++ frontend/public/scripts/leaderboard.js
@@ -15,8 +15,9 @@ setInterval(async () => {
   // Function to create leaderboard HTML
   function createLeaderboard(data) {
     // clear the leaderboard container
-    document.getElementById("leaderboard-container").innerHTML =
-      "<h1>Leaderboard</h1>";
+    document.getElementById(
+      "leaderboard-container"
+    ).innerHTML = `<h1 class="leaderboard-title">Leaderboard</h1>`;
     data.sort((a, b) => a.swings - b.swings);
 
     var leaderboardContainer = document.getElementById("leaderboard-container");
@@ -26,6 +27,9 @@ setInterval(async () => {
       listItem.textContent = `${index + 1}. ${player.playerName} - ${
         player.swings
       } swings`;
+      listItem.style.color = "#141716";
+      listItem.style.fontSize = "clamp(1rem, 3vw, 1.5rem)";
+      listItem.style.fontWeight = "500";
       leaderboardContainer.appendChild(listItem);
     });
   }
